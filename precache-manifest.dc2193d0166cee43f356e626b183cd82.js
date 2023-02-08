3bb6fdecf33c7122fca61d402949e18c031a051f
Timl163 <tim.luecking1@fh-bielefeld.de>
2023-02-08 14:54:13 +0100

    Updates

---
+++ precache-manifest.dc2193d0166cee43f356e626b183cd82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5031a81a61bab7afa279",
+    "url": "/bornit/static/css/main.57d6f831.chunk.css"
+  },
+  {
+    "revision": "5031a81a61bab7afa279",
+    "url": "/bornit/static/js/main.5031a81a.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/bornit/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/bornit/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "d5739c63ec98725a12ec",
+    "url": "/bornit/static/js/runtime~main.d5739c63.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/bornit/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "a090ea684bbfcc1bb8f3ea95ae8483f1",
+    "url": "/bornit/index.html"
+  }
+];
